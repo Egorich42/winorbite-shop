d02aed4fb9fbcd0bb13ba96e88aa33ebd4bfe88e
Egorich42 <e.g.hutter@gmail.com>
2017-07-18 16:52:03 +0300

    "

--- shop/static/JS/script.js
+++
@@ -1,5 +0,0 @@
-$('.top').on('click', function() {
-  $parent_box = $(this).closest('.box');
-  $parent_box.siblings().find('.bottom').slideUp();
-  $parent_box.find('.bottom').slideToggle(300, 'swing');
-});
